284ad5e6d2e001377775533f2d7a7c97970fa61b
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-06-19 21:12:28 +0000

    adding tailwindcss to application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
-import {useAuth0} from '@auth0/auth0-react'
 
 function App() {
-
-  const {loginWithPopup, loginWithRedirect, logout, user, isAuthenticated} = useAuth0()
-
   return (
     <div className="App">
-      <hi>Driver blah blah blah </hi>
-      <ul>
-        <li> 
-          <button onClick={loginWithPopup}> login with popup</button>
-        </li>
-        <li>
-          <button onClick={loginWithRedirect}> login with redirect</button>
-        </li>
-        <li>
-          <button onClick={logout}>
-            logout 
-          </button>
-        </li>
-      </ul>
+      kdfsfdfsldflksdfskfsdf
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Auth0Provider} from '@auth0/auth0-react'
 
 ReactDOM.render(  
   <React.StrictMode>
-    <Auth0Provider  
-        domain = {"dev--n-vtfyi.us.auth0.com"}
-        clientId = {"o6mly2wjxr0d8ErcIK2SxuWhUgo7g8Ao"}
-        redirectUri = {window.location.origin}
-    >
       <App />
-    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
