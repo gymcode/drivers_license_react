99f6ab2a43da3ea06d583349a00f916852309a6c
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-06-21 14:14:17 +0000

    pay some respects

--- src/App.js
+++ src/App.js
@@ -1,33 +1,66 @@
 import React from 'react'
-
 // importing components 
 import FormSection from "./components/form"
+import axios from 'axios'
+
+
+const accesssToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlZWRhNDA2Zi0zN2ZlLTRkYmUtODY2ZS02YTM0ZjEwM2RlMTkiLCJhdWQiOiI1ODdmZDZhOC00MTA3LTQ4ZTktOTQyYi1lZGE4MGI0NjNkZTkiLCJzdWIiOiI0Y2M0ZDIwOC0wYjEzLTQ0NTEtYjkzNy04ZTI2NDA5OGU0ZWMiLCJuYmYiOjAsInNjb3BlcyI6WyJ2ZXJpZmljYXRpb25fdmlldyIsInZlcmlmaWNhdGlvbl9saXN0IiwidmVyaWZpY2F0aW9uX2RvY3VtZW50IiwidmVyaWZpY2F0aW9uX2lkZW50aXR5Il0sImV4cCI6MzIwMTYyMjk5OCwiaWF0IjoxNjIzNzg2MTk4fQ.q7vJs44yS12Gw1qvKL_hqkTwG3neUdh7gXYZPlkAuDA'
+
+axios.interceptors.request.use(
+  config => {
+    config.headers.authorization = `Bearer ${accesssToken}`
+  }, 
+  error => {
+    return Promise.reject(error)
+  }
+)
 
 function App() {
   const [LicenseNumber, setLicenseNumber] = React.useState("")
   const [name, setName] = React.useState("")
   const [dob, setDob] = React.useState("")
-  
-  React.useEffect(()=>{
-    // post request options 
-    const reqOptions = {
-      method: "POST", 
-      headers: {'Content-type': 'application/json'}, 
-      body: JSON.stringify({
+
+  // function handleClick (e) {
+  //   e.preventDefault()
+
+  //   const Token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlZWRhNDA2Zi0zN2ZlLTRkYmUtODY2ZS02YTM0ZjEwM2RlMTkiLCJhdWQiOiI1ODdmZDZhOC00MTA3LTQ4ZTktOTQyYi1lZGE4MGI0NjNkZTkiLCJzdWIiOiI0Y2M0ZDIwOC0wYjEzLTQ0NTEtYjkzNy04ZTI2NDA5OGU0ZWMiLCJuYmYiOjAsInNjb3BlcyI6WyJ2ZXJpZmljYXRpb25fdmlldyIsInZlcmlmaWNhdGlvbl9saXN0IiwidmVyaWZpY2F0aW9uX2RvY3VtZW50IiwidmVyaWZpY2F0aW9uX2lkZW50aXR5Il0sImV4cCI6MzIwMTYyMjk5OCwiaWF0IjoxNjIzNzg2MTk4fQ.q7vJs44yS12Gw1qvKL_hqkTwG3neUdh7gXYZPlkAuDA'
+
+  //   const reqOptions = {
+  //     method: "POST", 
+  //     headers: {
+  //       'Content-type': 'application/json', 
+  //       'Authorization': Token
+  //     }, 
+  //     body: JSON.stringify({
+  //       id: LicenseNumber, 
+  //       full_name: name, 
+  //       date_of_birth: dob
+  //     })
+  //   }
+
+  //   fetch('https://api.appruve.co/v1/verifications/gh/driver_license', reqOptions)
+  //   .then(response => response.json )
+  //   .then(data => console.log(data))
+
+  //   console.log("eee")
+  // }
+
+  function handleClick(e){
+    e.preventDefault()
+
+
+    axios.post('https://api.appruve.co/v1/verifications/gh/driver_license', 
+      {
         id: LicenseNumber, 
         full_name: name, 
         date_of_birth: dob
-      })
-    }
-
-    const handleClick =fetch('https://api.appruve.co/v1/verifications/gh/driver_license', reqOptions)
-      .then(response => response.json )
-      .then(data => console.log(data))
-
-  }, [])
-
+      }
+    )
+  } 
+        
   return (
     <div className="flex justify-center">
+     <form onSubmit={handleClick}>
       <div>
             <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -74,7 +107,15 @@ function App() {
                     />
                 </div>
             </div>
+            <div>
+                <div className="mt-1 bg-red-300">
+                  <button type="submit" value="hello">
+                    hello
+                  </button>
+                </div>
+            </div>
         </div>
+     </form>
     </div>
   );
 }
