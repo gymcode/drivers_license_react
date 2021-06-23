f342be9af6f70cd6ad4caf1a8eb7684754382121
Kenneth Abrahams Lartey <kyleabs20@gmail.com>
2021-06-23 20:08:47 +0000

    what

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import React from 'react'
 import FormSection from "./components/form"
 import axios from 'axios'
 
-
-
-
 function App() {
   const [LicenseNumber, setLicenseNumber] = React.useState("")
-  const [name, setName] = React.useState("")
-  const [dob, setDob] = React.useState("")
   const [data, setData] = React.useState()
 
   console.log(data)
@@ -31,72 +26,54 @@ function App() {
 
     axios.post('https://api.appruve.co/v1/verifications/gh/driver_license', 
       {
-        id: LicenseNumber, 
-        full_name: name, 
-        date_of_birth: dob
+        id: LicenseNumber,        
       }
     ) 
     .then( ({data}) => setData(data))
   } 
         
   return (
-    <div className="flex justify-center">
-     <form onSubmit={handleClick}>
-      <div>
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                License Number 
-                </label>
-                <div className="mt-1">
-                    <input
-                        type="text"
-                        name="email"
-                        id="email"
-                        onChange = {e => setLicenseNumber(e.target.value)}
-                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                        placeholder="you@example.com"
-                    />
+    <div className={""}>
+      {
+        !data ? 
+        <div className={"grid grid-cols-2 gap-2 h-screen"}>
+          <div style={{background: "red"}} className={""}> 
+
+          </div>
+          <div className={""}>
+            <form onSubmit={handleClick}>
+              <div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        License Number 
+                        </label>
+                        <div className="mt-1">
+                            <input
+                                type="text"
+                                name="email"
+                                id="email"
+                                onChange = {e => setLicenseNumber(e.target.value)}
+                                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                placeholder="you@example.com"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="mt-1 bg-red-300">
+                          <button type="submit" value="hello">
+                            hello
+                          </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Name of license holder
-                </label>
-                <div className="mt-1">
-                    <input
-                        type="text"
-                        name="email"
-                        id="email"
-                        onChange = {e => setName(e.target.value)}
-                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                        placeholder="you@example.com"
-                    />
-                </div>
-            </div>
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Date of birth 
-                </label>
-                <div className="mt-1">
-                    <input
-                        type="text"
-                        name="email"
-                        id="email"
-                        onChange = {e => setDob(e.target.value)}
-                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                        placeholder="you@example.com"
-                    />
-                </div>
-            </div>
-            <div>
-                <div className="mt-1 bg-red-300">
-                  <button type="submit" value="hello">
-                    hello
-                  </button>
-                </div>
-            </div>
+            </form>
+          </div>
         </div>
-     </form>
+        :
+        <>
+          <div>dadadadasd</div>
+        </>
+      }
     </div>
   );
 }
